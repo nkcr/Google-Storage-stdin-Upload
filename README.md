f6894f891753e1a1a07d4d2bd098c3ddd60b735e
Noémien Kocher <nkcr.je@gmail.com>
2017-08-12 12:20:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+This script takes input from stdin and uploads it to Google Cloud storage.
+
+# Dependencies
+
+go version `1.8`
+
+# Installation
+
+## Go environment
+
+* Dowload go `1.8.3` from golang.org and extract if under `/usr/local` with 
+
+```
+tar -C /usr/local -xzf go1.8.3.$OS-$ARCH.tar.g
+```
+then link the bin executable 
+
+```
+ln -s /usr/local/go/bin/go /usr/local/bin
+```
+
+## Get dependencies and build
+
+* Download `storage-upload.go` and run `$ go get` within its parent folder.
+* finally run `go build storage-upload.go` to build the executable.
+
+# Usage
+
+The script takes as input the standard input **stdin**. The most convenient way is to use pipe.
+
+The script takes the followings parameters:
+
+```
+  -d string
+        Upload path, of form 'gs://bucket/folder/file.txt'
+  -help
+        Prints usage
+  -k string
+        Service key path
+  -p string
+        Create the bucket in the corresponding given projectID if it doesn't exist
+  -verbose
+        Outputs infos
+```
+
+**-d** and **-k** are always required. For the key, see next section *security*.
+
+Here is an example:
+
+```
+echo "Hello" | upload-sorage -k adfe43d.json -d gs://my_bucker/my_folder/file.txt
+```
+
+# Security
+
+The script asks for a **service account** key with the params `-k`. You can create a service account key via your console.cloud.google.com on the section **IAM & Admin**. Then click on **Service accounts**. There you can create a service account key and assigning roles to it. Roles needed are **Storage/Storage Object Creator** if you only want to create files without override, **Storage/Storage Object Admin** if you want to override files or **Storage/Storage Admin** if you want to create new buckets** (via `-p` params). Then don't forget to check **Furnish a new private key** in JSON format. This is the key you will need.
---
+++ storage-upload.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"log"
+	"os"
+	"strings"
+
+	"google.golang.org/api/option"
+
+	// Imports the Google Cloud Storage client package.
+	"cloud.google.com/go/storage"
+	"golang.org/x/net/context"
+)
+
+var verb bool
+
+func main() {
+	ctx := context.Background()
+
+	// Get args and process them
+	keypath := flag.String("k", "", "Service key path")
+	dest := flag.String("d", "", "Upload path, of form 'gs://bucket/folder/file.txt'")
+	help := flag.Bool("help", false, "Prints usage")
+	flag.BoolVar(&verb, "verbose", false, "Outputs infos")
+	projectID := flag.String("p", "", "Create the bucket in the corresponding given projectID if it doesn't exist")
+
+	flag.Parse()
+
+	switch {
+	case *help:
+		flag.PrintDefaults()
+		os.Exit(0)
+	case *keypath == "":
+		log.Fatal("You must provide the service key path with option '-k'")
+	case *dest == "":
+		log.Fatal("You must provide the destination path with option '-d'")
+	}
+
+	// Get bucket and path
+	slices := strings.SplitN(*dest, "/", 4)
+	if len(slices) < 4 {
+		log.Fatal("Wrong path, must be of kind 'gs://bucket/folder/file.txt'")
+	}
+	bucket := slices[2]
+	path := slices[3]
+	print(bucket)
+	print(path)
+
+	// Creates a client.
+	client, err := storage.NewClient(ctx, option.WithServiceAccountFile(*keypath))
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	// Object to interact with our bucket
+	bkt := client.Bucket(bucket)
+	if *projectID != "" {
+		if err := bkt.Create(ctx, *projectID, nil); err != nil {
+			log.Fatalf("Failed to create bucket: %v", err)
+		}
+	}
+
+	// Check the existence of the bucket
+	_, err = bkt.Attrs(ctx)
+	if err != nil {
+		log.Fatalf("Error with bucket: %v", err)
+	}
+
+	// Read from stdin
+	print("Reading data")
+	w := bkt.Object(path).NewWriter(ctx)
+	n, err := bufio.NewReader(os.Stdin).WriteTo(w)
+	if err != nil {
+		log.Fatalf("Uploading error: %v", err)
+	}
+	log.Printf("Sucessfully uploaded %d byte(s) of data", n)
+	if err := w.Close(); err != nil {
+		log.Fatalf("Close w2 error: %v", err)
+	}
+
+}
+
+func print(text string) {
+	if verb {
+		log.Println(text)
+	}
+}
